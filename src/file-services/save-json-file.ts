fb8b57b38337bce8ee9a0d5acf96a363fa2e24dd
Ron Lloyd <rlloyd2001@gmail.com>
2018-12-01 19:21:00 -0700

    code to save a json file when script is ran

--- src/file-services/save-json-file.ts
+++ src/file-services/save-json-file.ts
@@ -1,5 +1,12 @@
+import * as fs from 'fs';
+import moment from 'moment';
+
 export class SaveJsonFile {
   withDateInName(data: any) {
-    console.log('data', data);
+    const fileName = `prescription-updates-${moment().format('h-m-s')}.json`;
+    fs.writeFile(fileName,  JSON.stringify(data), 'utf-8', (err) => {
+      if (err) throw err;
+      console.log(`${fileName} created!`);
+    });
   }
 }
