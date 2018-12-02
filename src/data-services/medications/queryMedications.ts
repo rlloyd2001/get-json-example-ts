d2715e8f62c4621aa8eeaf20ba8efd4d573f1869
Ron Lloyd <rlloyd2001@gmail.com>
2018-12-02 11:42:45 -0700

    improving performance of http queries

--- src/data-services/medications/queryMedications.ts
+++ src/data-services/medications/queryMedications.ts
@@ -2,15 +2,29 @@ import { GetMedicationsInterface } from './getMedicationsInterface';
 import { MedicationInterface } from './medicationInterface';
 
 export class QueryMedications {
+  private static promises = {} as any;
+
   constructor(public getMedications: GetMedicationsInterface) {}
 
   byRxcui(rxcui: string): Promise<MedicationInterface[]> {
-    return this.getMedications.byRxcui(rxcui).then((medications) => {
+    const key = `r-${rxcui}`;
+    if (!!QueryMedications.promises[key]) { return QueryMedications.promises[key]; }
+    const promise = this.getMedications.byRxcui(rxcui).then((medications) => {
+      delete QueryMedications.promises[key];
       return medications.filter(medication => medication.active);
     });
+    QueryMedications.promises[key] = promise;
+    return promise;
   }
 
   forId(id: string): Promise<MedicationInterface> {
-    return this.getMedications.forId(id);
+    const key = `i-${id}`;
+    if (!!QueryMedications.promises[key]) { return QueryMedications.promises[key]; }
+    const promise = this.getMedications.forId(id).then((value) => {
+      delete QueryMedications.promises[key];
+      return value;
+    });
+    QueryMedications.promises[key] = promise;
+    return promise;
   }
 }
